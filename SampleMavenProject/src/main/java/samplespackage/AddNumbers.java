a9c336406741ca60bb1e99b646c9962aa539814c
Reema <reema_r@surgetechinc.in>
2026-04-11 22:55:16 -0400

    The project is committed

---
+++ SampleMavenProject/src/main/java/samplespackage/AddNumbers.java
@@ -0,0 +1,11 @@
+package samplespackage;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		
+		int n1 = 10;
+		int n2 = 5;
+		System.out.println("Sum of the numbers: " + (n1+n2));
+	}
+}
---
+++ SampleMavenProject/src/main/java/samplespackage/JustPrint.java
@@ -0,0 +1,10 @@
+package samplespackage;
+
+public class JustPrint {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is a sentence.");
+	}
+
+}
