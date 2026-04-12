4008a40bcf70028a49b8bca769c78d85c3b00907
Reema <reema_r@surgetechinc.in>
2026-04-11 23:02:57 -0400

    New commit - class: StringDispaly

---
+++ SampleMavenProject/src/main/java/samplespackage/StringDisplay.java
@@ -0,0 +1,11 @@
+package samplespackage;
+
+public class StringDisplay {
+
+	public static void main(String[] args) {
+		 String str= "Learning";
+		 System.out.println("String: " + str );
+
+	}
+
+}
